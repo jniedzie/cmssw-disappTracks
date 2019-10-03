3f1ff31a3c90edf9d45f8914b547bd1b5ee3cdae
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-10-03 09:26:15 +0200

    Removing crab project directory from the repo. Updating script path.

--- crabFriendTrees/crabConfig.py
+++ crabFriendTrees/crabConfig.py
@@ -7,7 +7,7 @@ config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/afs/cern.ch/work/j/jniedzie/private/CMSSW_9_4_6_patch1/src/CharginoAnalysis/CharginoAnalyzer/python/ConfFile_cfg.py'
+config.JobType.psetName = '/afs/cern.ch/work/j/jniedzie/private/disapp_tracks/friendTreeProducer/CMSSW_9_4_11/src/CharginoAnalysis/CharginoAnalyzer/python/ConfFile_cfg.py'
 
 config.Data.inputDataset = '/WToLNuJets_HT_200_400/jniedzie-crab_pickEvents_miniAOD-18783c0a07109245951450a1a4f55409/USER'
 config.Data.secondaryInputDataset = '/WToLNuJets_HT_200_400/jniedzie-crab_pickEvents_HLTDIGI-424e4485a07f26f554e82f829d793003/USER'
